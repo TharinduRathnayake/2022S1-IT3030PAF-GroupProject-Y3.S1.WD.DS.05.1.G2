10df10c8b41d6f747a4f00a0897004e1466ca048
TharinduRathnayake <89574455+IT20141288@users.noreply.github.com>
2022-04-25 12:50:41 +0530

    create customer service

--- CustomerService/src/main/java/model/ManageService.java
+++ CustomerService/src/main/java/model/ManageService.java
@@ -131,22 +131,3 @@ public String deleteItem(String notificationID)
  Connection con = connect();
  if (con == null)
  {return "Error while connecting to the database for deleting."; }
- // create a prepared statement
- String query = "delete from items where itemID=?";
- PreparedStatement preparedStmt = con.prepareStatement(query);
- // binding values
- preparedStmt.setInt(1, Integer.parseInt(notificationID));
- // execute the statement
- preparedStmt.execute();
- con.close();
- output = "Deleted successfully";
- }
- catch (Exception e)
- {
- output = "Error while deleting the item.";
- System.err.println(e.getMessage());
- }
- return output;
- }
-}
-
